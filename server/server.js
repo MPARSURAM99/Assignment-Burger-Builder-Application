507a4923cd9a8b1932da6ab3c5f5f853fb071207
PARSURAM MAHARANA <mparsuram99@gmail.com>
2026-05-28 12:58:45 +0530

    Cors origin issue fixing

--- server/server.js
+++ server/server.js
@@ -2,30 +2,29 @@ import express from "express";
 import mongoose from "mongoose";
 import cors from "cors";
 import dotenv from "dotenv";
+
+import orderRoutes from "./routes/orderRoutes.js";
+
 dotenv.config();
 
 const app = express();
 
 app.use(
   cors({
-    origin: ["https://assignment-burger-builder-applicati.vercel.app/"],
-    methods: ["GET", "POST", "PUT", "DELETE"],
-    credentials: true,
+    origin: "*",
+
+    methods: ["GET", "POST", "PUT", "DELETE", "OPTIONS"],
+
+    allowedHeaders: ["Content-Type", "Authorization"],
   }),
 );
+
+app.options("*", cors());
+
 app.use(express.json());
 
-mongoose
-  .connect(process.env.MONGO_URI)
-  .then(() => console.log("Mongo Connected"))
-  .catch((err) => console.log(err));
-
-// Import routes
-import orderRoutes from "./routes/orderRoutes.js";
-app.use("/api/orders", orderRoutes);
-
-const PORT = process.env.PORT || 5000;
-
-app.listen(PORT, () => {
-  console.log(`Running on ${PORT}`);
+app.get("/", (req, res) => {
+  res.send("Burger Builder Backend Running");
 });
+
+app.use("/api/orders", orderRoutes);
