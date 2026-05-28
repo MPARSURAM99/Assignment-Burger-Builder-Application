59d79a2d6854e4e118ea138b4e742e0128939528
PARSURAM MAHARANA <mparsuram99@gmail.com>
2026-05-28 12:41:43 +0530

    Api routes updated

--- client/src/services/api.js
+++ client/src/services/api.js
@@ -1,7 +1,7 @@
 import axios from "axios";
 
 const api = axios.create({
-  baseURL: "https://assignment-burger-builder-application.onrender.com/",
+  baseURL: "https://assignment-burger-builder-application.onrender.com/api",
 });
 
 export default api;
